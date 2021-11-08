3459f364575785cc6290b21d78f4c8c70e56f190
lingayya0124 <lingayyah38@gmail.com>
2021-11-08 11:18:20 +0530

    navbar working

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,57 @@ export default {
         menuOptionsLeft: [
           {
             type: "link",
-            text: "Why Dunder Mifflin",
+            text: "Dashboard",
+            path: { name: "pricing" },
+            iconLeft: '<i class="fa fa-user-secret" aria-hidden="true"></i>',
+          },
+          {
+            type: "link",
+            text: "Home",
+            path: { name: "pricing" },
+            iconLeft: '<i class="fa  fas fa-home" aria-hidden="true"></i>',
+          },
+          {
+            type: "link",
+            text: "Assets",
+            path: { name: "pricing" },
+            iconLeft: '<i class="fa fas fa-file" aria-hidden="true"></i>',
+          },
+          {
+            type: "link",
+            text: "CvesList",
+            path: { name: "pricing" },
+            iconLeft: '<i class="fa fas fa-list" aria-hidden="true"></i>',
+          },
+          {
+            type: "link",
+            text: "Scan Cycle",
+            path: { name: "pricing" },
+            iconLeft: '<i class="fa fas fa-history" aria-hidden="true"></i>',
+          },
+
+          {
+            type: "link",
+            text: "Admin",
             arrowColor: "#659CC8",
+            iconLeft: '<i class="fa fas fa-user" aria-hidden="true"></i>',
             subMenuOptions: [
               {
                 isLinkAction: true,
                 type: "link",
-                text: "About",
-                subText:
-                  "Stupid corporate wet blankets. Like booze ever killed anyone.",
+                text: "Users",
+
                 path: { name: "about" },
-                iconLeft: '<i class="fa fa-star fa-fw"></i>',
+                iconLeft: '<i class="fa fa-users"></i>',
               },
               {
                 type: "hr",
               },
               {
                 type: "link",
-                text: "Locations",
-                subText: "You're a presentation tool!",
+                text: "Contacts",
+                iconLeft: '<i class=" fa fas fa-phone-alt"></i>',
+
                 path: { name: "locations" },
                 arrowColor: "#659CC8",
               },
@@ -49,59 +81,26 @@ export default {
               },
               {
                 type: "link",
-                text: "Blog",
-                subText:
-                  "I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.",
+                text: "Invoice",
+                iconLeft:
+                  '<i class=" fas fa-file-invoice" aria-hidden="true"></i>',
+
                 path: { name: "blog" },
               },
             ],
           },
           {
             type: "link",
-            text: "Contact",
+            text: "I am Parham",
+            iconLeft: '<i class="fa fa-user"></i>',
+
             subMenuOptions: [
               {
                 type: "link",
-                text: "Customer Service",
+                text: "Logout",
                 path: { name: "customer-service" },
               },
-              {
-                type: "link",
-                text: "Accounting",
-                path: { name: "accounting" },
-              },
-              {
-                type: "hr",
-              },
-              {
-                type: "link",
-                text: "Reception",
-                path: { name: "reception" },
-                iconLeft:
-                  '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
-              },
             ],
-          },
-          {
-            type: "link",
-            text: "Pricing",
-            path: { name: "pricing" },
-            iconRight: '<i class="fa fa-long-arrow-right fa-fw"></i>',
-          },
-        ],
-        menuOptionsRight: [
-          {
-            type: "button",
-            text: "Signup",
-            path: { name: "signup" },
-            class: "button-red",
-          },
-          {
-            type: "button",
-            text: "Login",
-            path: { name: "login" },
-            iconRight:
-              '<svg id="i-arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M22 6 L30 16 22 26 M30 16 L2 16" /> </svg>',
           },
         ],
       },
@@ -110,13 +109,146 @@ export default {
 };
 </script>
 
+
+
 <style lang="scss">
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,500,700");
+
 .vnb {
+  background: #00203d;
+  color: #ddd;
   .button-red {
     background: #ff3b30;
 
     &:hover {
       background: darken(#ff3b30, 10%);
+    }
+  }
+}
+html {
+  width: 100%;
+  font-size: 18px;
+  color: #fff;
+}
+body {
+  margin: 0;
+  height: 100%;
+  height: 100vh;
+  width: 100%;
+  font-family: "Montserrat", sans-serif;
+}
+#app {
+  height: 100%;
+  height: 100vh;
+  width: 100%;
+  line-height: 1.5;
+}
+.code-text {
+  background: #010645;
+  border: 1px solid #ddd;
+  padding: 10px 20px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+@media (min-width: 992px) {
+  .code-text {
+    margin-bottom: 0;
+  }
+}
+.btn {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+.github-corner:hover .octo-arm {
+  animation: octocat-wave 560ms ease-in-out;
+}
+@keyframes octocat-wave {
+  0%,
+  100% {
+    transform: rotate(0);
+  }
+  20%,
+  60% {
+    transform: rotate(-25deg);
+  }
+  40%,
+  80% {
+    transform: rotate(10deg);
+  }
+}
+@media (max-width: 500px) {
+  .github-corner:hover .octo-arm {
+    animation: none;
+  }
+  .github-corner .octo-arm {
+    animation: octocat-wave 560ms ease-in-out;
+  }
+}
+.main-content-section {
+  padding: 45px 0;
+  background: #010645;
+  min-height: 100%;
+}
+//
+// vue-navigation-bar customization below
+//
+.main-navbar-section {
+  background: #010645;
+}
+.vnb {
+  font-family: "Montserrat", sans-serif;
+  @media (min-width: 992px) {
+    .button-red {
+      background: #ff3b30;
+      &:hover {
+        background: darken(#ff3b30, 10%);
+      }
+    }
+  }
+  &__menu-options {
+    &__option {
+      &__link {
+        color: #ddd;
+        &:hover {
+          color: #ddd;
+        }
+      }
+      &__button {
+        &__icon {
+          svg {
+            margin-top: -3px;
+          }
+        }
+      }
+    }
+  }
+  &__popup {
+    &__bottom {
+      &__menu-options {
+        &__option {
+          &__link {
+            &__icon {
+              svg {
+                margin-top: -4px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .custom-section-content {
+    width: 100%;
+    @media (min-width: 568px) {
+      width: 50%;
+    }
+    @media (min-width: 992px) {
+      width: 15%;
+    }
+    @media (min-width: 1200px) {
+      width: 20%;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import VueNavigationBar from "vue-navigation-bar";
 Vue.component("vue-navigation-bar", VueNavigationBar);
 Vue.config.productionTip = false;
-
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
